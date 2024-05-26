6b34dbe7378eb6ce089c5c8ae5fd0b0982609418
rchaconalcantara <hola.hack09@gmail.com>
2024-05-26 01:35:44 -0400

    Improvements, adding voice for call patients and size of turns

--- resources/views/livewire/shifts.blade.php
+++ resources/views/livewire/shifts.blade.php
@@ -3,14 +3,14 @@
     <div class="row justify-content-around" wire:poll.100ms>
 
         @foreach ($shifts as $shift)
-            <div class="col-10 d-flex justify-content-around p-2 mb-3 border shadow rounded">
+            <div class="col-12 d-flex justify-content-around p-1 mb-3 border shadow rounded">
                 <div class="col-6">
                     <button type="submit" class="container border-0 p-2 rounded shadow bg-primary text-center text-white">
-                        <h1><b>{{ $shift['code'] }}</b></h1>
+                        <h1 style="font-size: 80px;"><b>{{ $shift['code'] }}</b></h1>
                     </button>
                 </div>
                 <div class="col-6 d-flex justify-content-center align-items-center">
-                    <h2><b>Posición {{ $shift['window'] ?? '0' }}</b></h2>
+                    <h1><b>Posición {{ $shift['window'] ?? '0' }}</b></h1>
                 </div>
             </div>
         @endforeach
